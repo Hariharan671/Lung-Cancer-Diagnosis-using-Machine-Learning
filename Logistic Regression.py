703f295657de9193b62bdb9dca8d86b50d0b1fa0
Hariharan671 <haran8861@gmail.com>
2020-06-14 20:39:52 +0530

    initial commit

---
+++ Logistic Regression.py
@@ -0,0 +1,62 @@
+#
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#
+dataset =pd.read_csv('lung_cancer_examples.csv')
+X=dataset.iloc[:,2:6].values
+y=dataset.iloc[:,6].values
+#
+from sklearn.preprocessing import LabelEncoder
+y= LabelEncoder().fit_transform(y)
+#
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=0.5,random_state=0)
+#
+from sklearn.preprocessing import StandardScaler
+sc_X= StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+#
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train,y_train)
+#
+Y_pred = classifier.predict(X_test)
+#
+from  sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,Y_pred)
+#
+print("Accuracy of the Model is : ",(cm[0][0]+cm[1][1])*100/(cm[0][0]+cm[1][1]+cm[0][1]+cm[1][0]))
+print("Precision of the Model is : ",(cm[0][0])*100/(cm[0][0]+cm[1][0]))
+print("Recall of the Model is : ",(cm[0][0])*100/(cm[0][0]+cm[0][1]))
+#
+from sklearn.decomposition import PCA
+pca= PCA(n_components=2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance= pca.explained_variance_ratio_
+#
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train,y_train)
+#
+Y_pred = classifier.predict(X_test)
+#
+from matplotlib.colors import ListedColormap
+X_set,y_set=X_test,y_test
+X1,X2= np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),alpha=0.75,cmap =ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j,0],X_set[y_set==j,1],c=ListedColormap(('red','green'))(i),label=j)
+plt.title('Logical Regression for training Set')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.legend()
+plt.show()
+
+
+
+
